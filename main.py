2299400f33ca2333a6fdb1665a1f51390976b4b4
gilbertf15 <gilbertcorreia18@gmail.com>
2025-07-24 22:49:07 -0300

    adicionando template na rota home

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import uvicorn
 import requests
 from app.api.routes.routes import routercontabil
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from  fastapi.templating import Jinja2Templates
 
 class APIContabil(FastAPI):
     def __init__(self, debug = True, title = "APICONTABIL", summary = None
@@ -15,13 +18,15 @@ class IncludeRoutes:
         app.include_router(routercontabil)
 
 app = APIContabil()
-@app.get("/")
-async def home_test():
-    try:
-        return {"BEM VIDO":
-            "BEM VINDO A APICONTABIL"
-            }
-    except requests.exceptions.HTTPError:
+
+app.mount("/app/frontend/static", StaticFiles(directory="app/frontend/static"), name="static")
+templates = Jinja2Templates(directory="app/frontend/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home_test(request: Request):
+    
+    return templates.TemplateResponse("home.html", {'request': request})
+    """except requests.exceptions.HTTPError:
         return {
             "ERROR": "Error API"
         }
@@ -29,7 +34,7 @@ async def home_test():
     except requests.exceptions.ConnectionError:
         return {
            "ERROR":  "Error connection"
-        }
+        }"""
 IncludeRoutes.main_include_routes(app)
 if __name__ == '__main__':
     uvicorn.run(app, port=8000)
